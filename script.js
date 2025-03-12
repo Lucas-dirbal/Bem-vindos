496d65b69a5f6afa43b49c54b0b5cd2a60643e10
Lucas Dircksen Balcevicz <145569604+Lucas-dirbal@users.noreply.github.com>
2025-03-12 00:00:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,26 +5,18 @@ window.onload = () => {
     const sucesso = document.querySelector('.sucesso');
 
     setTimeout(() => {
-        familia.style.top = '0px';
-        familia.style.left = '0px';
-        familia.style.opacity = 1;
+        familia.style.opacity = 1;  // Inicia a animação
     }, 500);
 
     setTimeout(() => {
-        escola.style.top = '0px';
-        escola.style.left = '150px';
         escola.style.opacity = 1;
     }, 1000);
 
     setTimeout(() => {
-        parceria.style.top = '150px';
-        parceria.style.left = '0px';
         parceria.style.opacity = 1;
     }, 1500);
 
     setTimeout(() => {
-        sucesso.style.top = '150px';
-        sucesso.style.left = '150px';
         sucesso.style.opacity = 1;
     }, 2000);
 };
